91e48cddbb2a4483e1c3b3263de1052a10c0ca16
vellya-cloud <vellyalfadila@gmail.com>
2025-05-09 20:22:56 +0700

    checkhbox

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@ const addTask = () => {
 
   <ul>
     <li v-for="task in tasks" :key="task.id">
+      <input type="checkbox" v-model="task.completed" />
       {{ task.text }}
     </li>
   </ul>
